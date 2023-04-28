5ece15a50270a3dc46a5b8da9c7af934154c9d14
micheal876 <edw20011@byui.edu>
2023-04-27 19:35:24 -0500

    commit new

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -2,11 +2,11 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error('Bad Response');
   }
 }
 
-export async function getData(category = "tents") {
+export async function getData(category = 'tents') {
   const res = await fetch(`../json/${category}.json`);
   const data = await convertToJson(res);
   return data;
